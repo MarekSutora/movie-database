85bc550543569343c4c118f3ce79fb1a7bbbb9bb
Marek Šútora <marek.sutora@gmail.com>
2024-07-24 22:02:31 +0200

    non lazy details

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,12 @@ import Layout from "./components/layout/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider as JotaiProvider } from "jotai";
+import DetailsPage from "./pages/DetailsPage/DetailsPage";
 
 const SearchPage = React.lazy(() => import("./pages/SearchPage/SearchPage"));
 const FavouritesPage = React.lazy(
   () => import("./pages/FavouritesPage/FavouritesPage")
 );
-const DetailsPage = React.lazy(() => import("./pages/DetailsPage/DetailsPage"));
 
 const router = createBrowserRouter(
   [
@@ -29,9 +29,7 @@ const router = createBrowserRouter(
       path: "/movie-details",
       element: (
         <Layout>
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <DetailsPage />
-          </React.Suspense>
+          <DetailsPage />
         </Layout>
       ),
     },
